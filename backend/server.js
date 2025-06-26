c4b01a2a8fbb3f938c01de2cd2eb2776e25a128f
Jason Lin <lin1jason8@outlook.com>
2025-06-26 11:37:52 +0900

    Setting up redis connection through upstash

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { createServer } from "http";
-import { specs, swaggerUi } from './swagger.js';
+import { specs, swaggerUi } from "./swagger.js";
+import { Redis } from "@upstash/redis";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -10,11 +11,15 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_URL,
+  token: process.env.UPSTASH_REDIS_TOKEN,
+});
+
 // Middleware
 app.use(cors()); // Enable cross-origin resource sharing
 app.use(express.json()); // Automatically parse incoming JSON request bodies
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));  // Set up api endpoint for swagger ui
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs)); // Set up api endpoint for swagger ui
 
 /**
  * @swagger
@@ -28,7 +33,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));  // Set up api en
  *        content:
  *          text/plain:
  *            schema:
- *              type: string   
+ *              type: string
  */
 app.get("/", (req, res) => {
   res.send("Backend is ready.");
@@ -36,6 +41,15 @@ app.get("/", (req, res) => {
 
 // Starting the backend server
 const startServer = async () => {
+  // Test redis connection
+  try {
+    await redis.set('connection_test', 'Redis connection successful!');
+    const testValue = await redis.get('connection_test');
+    console.log(testValue);
+  } catch (error) {
+    console.error('Redis connection failed');
+  }
+  
   server.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
   });
