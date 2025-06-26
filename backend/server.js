eb25c105e4af9aaa4e9ed8e26af0863f5729b861
Jason Lin <lin1jason8@outlook.com>
2025-06-26 11:12:53 +0900

    Modified the structure of server
    Moved swagger set up into a separate file

--- backend/server.js
+++ backend/server.js
@@ -2,60 +2,42 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { createServer } from "http";
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
+import { specs, swaggerUi } from './swagger.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = createServer(app);
-// swagger setup
-// http://<app_host>:<app_port>/api-docs
-const swaggerOptions = {
-  swaggerDefinition: {
-    myapi: '3.0.0',
-    info: {
-      title: 'API',
-      version: '1.0.0',
-      description: 'API documentation',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], 
-};
-
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-
-
-// Sample route
-app.get('/api/hello', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
 
 // Middleware
 app.use(cors()); // Enable cross-origin resource sharing
 app.use(express.json()); // Automatically parse incoming JSON request bodies
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));  // Set up api endpoint for swagger ui
 
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    summary: Access home page
+ *    description: Returns a simple message indicating the backend is ready
+ *    responses:
+ *      200:
+ *        descrition: Backend status message
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: string   
+ */
 app.get("/", (req, res) => {
   res.send("Backend is ready.");
 });
 
 // Starting the backend server
-// const startServer = async () => {
-//   server.listen(PORT, () => {
-//     console.log(`Server started at http://localhost:${PORT}`);
-//   });
-// };
-// startServer();
+const startServer = async () => {
+  server.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+  });
+};
+startServer();
