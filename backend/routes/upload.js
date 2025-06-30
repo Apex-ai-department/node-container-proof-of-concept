680f80f6dc751c0bd8ea7f8885fa77479e4212be
jannadungao <jannadungao2@gmail.com>
2025-06-30 14:53:56 +0900

    Setup Posttgres SQL in docker

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { upload } from "../config/multer.js";
+import { generateUploadUrls } from "../controllers/generate_upload_urls.js";
+
 const router = express.Router();
 
 // Prefix: /api/upload
-//router.post("/", upload.array('files', 200), handleUpload);
-router.get("/url", get);
+router.post("/", upload.array('files', 200), handleUpload);
+router.get("/url", generateUploadUrls);
 export default router;
