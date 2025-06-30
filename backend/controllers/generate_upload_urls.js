afe834ceb7888c9621d80552ff01af404923cbea
jannadungao <jannadungao2@gmail.com>
2025-06-30 16:26:04 +0900

    Edit packages

--- backend/controllers/generate_upload_urls.js
+++ backend/controllers/generate_upload_urls.js
@@ -4,7 +4,7 @@ import path from "path";
 import { s3Client } from "../config/s3.js";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { generateUploadUrls } from "./generateUploadUrls.js";
+// import { generateUploadUrls } from "./generateUploadUrls.js";
 dotenv.config();
 
 const bucketName = process.env.S3_BUCKET_NAME;
