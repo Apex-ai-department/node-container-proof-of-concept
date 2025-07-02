8ef81475962aaac164895300641b32564323d349
jannadungao <jannadungao2@gmail.com>
2025-07-02 13:29:15 +0900

    Delete jobs.js

--- backend/routes/jobs.js
+++
@@ -1,9 +0,0 @@
-// routes/jobs.js
-import express from "express";
-import { saveJobAndResults } from "../controllers/jobAndResultsController.js";
-
-const router = express.Router();
-
-router.post("/save", saveJobAndResults);
-
-export default router;
